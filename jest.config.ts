f5becaf3bb16d99f2dba408d1ea48a5d4f23ddb5
himanshu-pahuja <himanshu.pahuja@zeeve.io>
2023-04-28 16:29:53 +0530

    REAVAX-21 | Added jest config & Transaction Test Cases

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/src", "<rootDir>/tests"],
+    testMatch: [
+      "**/__tests__/**/*.+(ts|tsx|js)",
+      "**/?(*.)+(spec|test).+(ts|tsx|js)",
+    ],
+    transform: {
+      "^.+\\.(ts|tsx)$": "ts-jest",
+    },
+    transformIgnorePatterns: ["<rootDir>/node_modules/"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  };
